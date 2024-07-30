91a9ad2bd040ae537a5ebe7ef166429b43a5912b
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2024-07-30 13:57:50 -0400

    feat: 💫 add constants and functions

---
+++ src/constants/urls.ts
@@ -0,0 +1,11 @@
+export const URL_AUTH = 'http://localhost:8080/login'
+export const URL_USER_CREATE = 'http://localhost:8080/collaborator/register'
+
+export const URL_USER_ID = 'http://localhost:8080/user/{userId}'
+export const URL_USER_ALL = 'http://localhost:8080/user/all'
+
+export const URL_USERS = 'http://localhost:8080/collaborator'
+export const URL_USERS_CREATE = 'http://localhost:8080/users'
+
+
+export const URL_ACTIVITY = 'http://localhost:8080/activity'
